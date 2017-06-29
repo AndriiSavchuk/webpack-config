d2e132a9b122bad12c23e530735a0ffd039f8f3f
Andrii Savchuk <andriksr@gmail.com>
2017-06-29 19:30:50 +0200

    rebuild sass

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,47 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
-    entry: ['./src/app.js'],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.sass$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader','sass-loader'],
-                    publicPath: '/dist'
-                })
-            },
-            {
-                use: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devServer: {
-        contentBase: './dist',
-        inline: true,
-        stats: 'errors-only',
-        open: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack Project',
-            // minify: {
-            //     collapseWhitespace: true
-            // },
-            hash: true,
-            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
-        }),
-        new ExtractTextPlugin({
-            filename: 'style.css',
-            disable: false,
-            allChunks: true
+  entry: ['./src/app.js'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
         })
+      },
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
     ]
+  },
+  devServer: {
+    contentBase: './dist',
+    inline: true,
+    stats: 'errors-only',
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Project',
+      // minify: {
+      //     collapseWhitespace: true
+      // },
+      hash: true,
+      template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      disable: false,
+      allChunks: true
+    })
+  ]
 }
